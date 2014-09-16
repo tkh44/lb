d7b1c44b66873cb2b0ae03950ff82568d6bc01fb
Kye Hohenberger <kye.hohenberger@gmail.com>
2014-09-16 02:34:04 -0500

    Updated home page for easier debugging for time being

--- controllers/home.js
+++ controllers/home.js
@@ -1,10 +1,41 @@
-/**
- * GET /
- * Home page.
- */
+var async = require('async');
+var League = require('../models/League');
+var Team = require('../models/Team');
+var Player = require('../models/Player');
+var _ = require('lodash');
+
+
+var getUserData = function(userId, cb) {
+	async.parallel({
+		leagues: function(cb) {
+			League.getByManager(userId, cb);
+		},
+		teams: function(cb) {
+			Team.getByManager(userId, cb);
+		},
+		players: function(cb) {
+			Player.getByUser(userId, cb);
+		}
+	}, cb);
+};
 
 exports.index = function(req, res) {
-  res.render('home', {
-    title: 'Home'
-  });
+	var responseData = {
+		title: 'Home'
+	};
+
+	if (req.user) {
+		getUserData(req.user.id, function(err, results) {
+			if (err) return err;
+			res.render('home', _.assign(responseData, {
+				leagues: results.leagues,
+				teams: results.teams,
+				players: results.players
+			}));
+		});
+	} else {
+		res.render('home', responseData);
+	}
+
+
 };
